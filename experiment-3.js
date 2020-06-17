6413535e3f44d7a207a370f7986ff411d1102a6a
rardosa <34051984+rardosa@users.noreply.github.com>
2020-06-16 23:58:52 -0700

    rehash
    so many changes

---
+++ experiment-3.js
@@ -0,0 +1,23 @@
+$(document).ready(function (){
+    $('#project-nav').click(function() {
+        $('html, body').animate({
+            scrollTop : $('#marker-1').offset().top - 130
+        }, 1000);
+    })
+})
+
+$(document).ready(function (){
+    $('#about-nav').click(function() {
+        $('html, body').animate({
+            scrollTop : $('#marker-2').offset().top - 130
+        }, 1000);
+    })
+})
+
+$(document).ready(function (){
+    $('#button-navigate').click(function() {
+        $('html, body').animate({
+            scrollTop : $('#marker-1').offset().top - 130
+        }, 1000);
+    })
+})
